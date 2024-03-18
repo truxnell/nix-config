8f96fbb0c5369f111ecce660e714ca8267062359
truxnell <19149206+truxnell@users.noreply.github.com>
2024-03-18 20:02:10 +1100

    feat: add direnv shell

--- shell.nix
+++ shell.nix
@@ -1,26 +1,18 @@
-# Shell for bootstrapping flake-enabled nix and other tooling
-{
-  pkgs ?
-  # If pkgs is not defined, instanciate nixpkgs from locked commit
-  let
-    lock =
-      (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+# Shell for bootstrapping flake-enabled nix and home-manager
+{ pkgs ? let
+    # If pkgs is not defined, instantiate nixpkgs from locked commit
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
     nixpkgs = fetchTarball {
       url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
       sha256 = lock.narHash;
     };
+    system = builtins.currentSystem;
+    overlays = [ ]; # Explicit blank overlay to avoid interference
   in
-    import nixpkgs {overlays = [];},
-  ...
-}:
-pkgs.mkShell {
-  NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
-  nativeBuildInputs = with pkgs; [
-    nixpkgs-fmt
-    nil
-    sops
-    pre-commit
-    go-task
-    alejandra
-  ];
+  import nixpkgs { inherit system overlays; }
+, ...
+}: pkgs.mkShell {
+  # Enable experimental features without having to specify the argument
+  NIX_CONFIG = "experimental-features = nix-command flakes";
+  nativeBuildInputs = with pkgs; [ nix home-manager git nil nixpkgs-fmt go-task sops ];
 }
