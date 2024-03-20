4b4a5c0f967769cbc756f435fdaf29b628b0b6f9
truxnell <19149206+truxnell@users.noreply.github.com>
2024-03-21 09:44:57 +1100

    feat: add substituters

--- nixos/hosts/common/nixos/nix.nix
+++ nixos/hosts/common/nixos/nix.nix
@@ -7,7 +7,18 @@
   nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      trusted-users = [ "root" "@wheel" ];
+      auto-optimise-store = lib.mkDefault true;
+      warn-dirty = false;
+
     };
+
     gc = {
       automatic = true;
       dates = "weekly";
