57bf5ad842fc39cb847f3954faa807f1c3e1c7c6
Truxnell <9149206+truxnell@users.noreply.github.com>
2024-03-31 23:12:17 +1100

    fix: fix bootstrap nix

--- nixos/hosts/bootstrap_iso/default.nix
+++ nixos/hosts/bootstrap_iso/default.nix
@@ -22,6 +22,6 @@
     ];
   };
 
-  system.stateVersion = "23.11";
+  nixpkgs.hostPlatform = "x86_64-linux";
 
 }
--- nixos/hosts/bootstrap_rpi4/default.nix
+++ nixos/hosts/bootstrap_rpi4/default.nix
@@ -1,70 +1,17 @@
 { config, pkgs, lib, nixos-hardware, ... }:
 
 {
-  imports = [
-    # nixos-hardware.nixosModules.raspberry-pi-4
-    ../../common/nixos/openssh.nix
-  ];
+  mySystem.services = {
 
-  nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      trusted-users = [ "root" "@wheel" ];
-    };
+    openssh.enable = true;
   };
 
-  nixpkgs = {
-
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
-
-  boot = {
-    initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
-    # ttyAMA0 is the serial console broken out to the GPIO
-    kernelParams = [
-      "8250.nr_uarts=1"
-      "console=ttyAMA0,115200"
-      "console=tty1"
-    ];
-    loader = {
-      grub.enable = false;
-      raspberryPi = {
-        version = 4;
-      };
-    };
-  };
-
-  # # https://nixos.wiki/wiki/NixOS_on_ARM/Raspberry_Pi_4
-  # hardware = {
-  #   raspberry-pi."4".apply-overlays-dtmerge.enable = true;
-  #   deviceTree = {
-  #     enable = true;
-  #     filter = "*rpi-4-*.dtb";
-  #   };
-  # };
+  networking.hostName = "dns02"; # Define your hostname.
+  networking.useDHCP = lib.mkDefault true;
 
   console.enable = false;
 
-  environment.systemPackages = with pkgs; [
-    libraspberrypi
-    raspberrypi-eeprom
-    ssh-to-age
-    vim
-    git
-    curl
-    wget
-    dnsutils
-  ];
-
-  networking = {
-    hostName = "nixos";
-    wireless.enable = false;
-    networkmanager.enable = false;
-  };
+  swapDevices = [ ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.truxnell = {
@@ -77,13 +24,6 @@
     ];
   };
 
-  # Free up to 1GiB whenever there is less than 100MiB left.
-  nix.extraOptions = ''
-    min-free = ${toString (100 * 1024 * 1024)}
-    max-free = ${toString (1024 * 1024 * 1024)}
-  '';
   nixpkgs.hostPlatform = "aarch64-linux";
-
-  system.stateVersion = "23.11";
 
 }
