6751703154d10f7c7fe68a34caadb6f8277743a5
Truxnell <9149206+truxnell@users.noreply.github.com>
2024-05-05 20:29:49 +1000

    hax

---
+++ nixos/modules/nixos/containers/maloja/default.nix
@@ -0,0 +1,161 @@
+{ lib
+, config
+, pkgs
+, ...
+}:
+with lib;
+let
+  cfg = config.mySystem.${category}.${app};
+  app = "maloja";
+  category = "containers";
+  description = "Music scrobbler";
+  image = "krateng/maloja:3.2.2@sha256:c76bca4be5b382118df83a8ab6be5b7a2c69622503a24f0948d04ff22caacde2";
+  user = "568"; #string
+  group = "568"; #string
+  port = 42010; #int
+  appFolder = "/var/lib/${app}";
+  # persistentFolder = "${config.mySystem.persistentFolder}/var/lib/appFolder";
+  host = "${app}" + (if cfg.dev then "-dev" else "");
+  url = "${host}.${config.networking.domain}";
+in
+{
+  options.mySystem.${category}.${app} =
+    {
+      enable = mkEnableOption "app";
+      addToHomepage = mkEnableOption "Add app to homepage" // { default = true; };
+      monitor = mkOption
+        {
+          type = lib.types.bool;
+          description = "Enable gatus monitoring";
+          default = true;
+        };
+      prometheus = mkOption
+        {
+          type = lib.types.bool;
+          description = "Enable prometheus scraping";
+          default = true;
+        };
+      addToDNS = mkOption
+        {
+          type = lib.types.bool;
+          description = "Add to DNS list";
+          default = true;
+        };
+      dev = mkOption
+        {
+          type = lib.types.bool;
+          description = "Development instance";
+          default = false;
+        };
+      backup = mkOption
+        {
+          type = lib.types.bool;
+          description = "Enable backups";
+          default = true;
+        };
+
+
+
+    };
+
+  config = mkIf cfg.enable {
+
+    ## Secrets
+    # sops.secrets."category/app/env" = {
+    #   sopsFile = ./secrets.sops.yaml;
+    #   owner = user;
+    #   group = group;
+    #   restartUnits = [ "app.service" ];
+    # };
+
+    users.users.truxnell.extraGroups = [ group ];
+
+
+    # Folder perms - only for containers
+    # systemd.tmpfiles.rules = [
+    # "d persistentFolder/ 0750 user group -"
+    # ];
+
+    environment.persistence."${config.mySystem.system.impermanence.persistPath}" = lib.mkIf config.mySystem.system.impermanence.enable {
+      directories = [{ directory = appFolder; inherit user; inherit group; mode = "750"; }];
+    };
+
+
+    ## service
+    # services.test= {
+    #   enable = true;
+    # };
+
+    ## OR
+
+    virtualisation.oci-containers.containers = config.lib.mySystem.mkContainer {
+      inherit app image;
+      user = "0";
+      group = "0";
+      env = {
+        MALOJA_DATA_DIRECTORY = "/config";
+      };
+    };
+
+
+    # homepage integration
+    mySystem.services.homepage.infrastructure = mkIf cfg.addToHomepage [
+      {
+        app = {
+          icon = "app.svg";
+          href = "https://${url}";
+          inherit description;
+        };
+      }
+    ];
+
+    ### gatus integration
+    mySystem.services.gatus.monitors = mkIf cfg.monitor [
+      {
+        name = app;
+        group = "category";
+        url = "https://${url}";
+        interval = "1m";
+        conditions = [ "[CONNECTED] == true" "[STATUS] == 200" "[RESPONSE_TIME] < 50" ];
+      }
+    ];
+
+    ### Ingress
+    services.nginx.virtualHosts.${url} = {
+      forceSSL = true;
+      useACMEHost = config.networking.domain;
+      locations."^~ /" = {
+        proxyPass = "http://${app}:${builtins.toString port}";
+        extraConfig = "resolver 10.88.0.1;";
+      };
+    };
+
+    ### firewall config
+
+    # networking.firewall = mkIf cfg.openFirewall {
+    #   allowedTCPPorts = [ port ];
+    #   allowedUDPPorts = [ port ];
+    # };
+
+    ### backups
+    warnings = [
+      (mkIf (!cfg.backup && config.mySystem.purpose != "Development")
+        "WARNING: Backups for app are disabled!")
+    ];
+
+    services.restic.backups = mkIf cfg.backup (config.lib.mySystem.mkRestic
+      {
+        inherit app user;
+        paths = [ appFolder ];
+        inherit appFolder;
+      });
+
+
+    # services.postgresqlBackup = {
+    #   databases = [ app ];
+    # };
+
+
+
+  };
+}
