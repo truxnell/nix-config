df86ac3a0fa885412c84604c0f52a9e7243dffff
Truxnell <9149206+truxnell@users.noreply.github.com>
2024-05-14 12:22:15 +1000

    feat: qbtools

--- nixos/modules/nixos/containers/qbittorrent/qbtools.nix
+++ nixos/modules/nixos/containers/qbittorrent/qbtools.nix
@@ -2,29 +2,29 @@
 let
   cfg = config.mySystem.services.qbittorrent;
   image = "ghcr.io/buroa/qbtools:v0.15.0@sha256:067a68a0c7b2f522b7527e7bb48cf18614d46c16fcbcd16561d1bbc7f7f983fd";
+
 in
 with lib;
 {
   config = mkIf cfg.enable {
 
     ## Secrets
-    sops.secrets."services/${app}/config.yaml" = {
+    sops.secrets."services/qbittorrent/config.yaml" = {
       sopsFile = ./secrets.sops.yaml;
       owner = config.users.users.kah.name;
       inherit (config.users.users.kah) group;
-      restartUnits = [ "podman-${app}.service" ];
     };
 
-    systemd.timers."qbit-tag" = {
+    systemd.timers."qbtools-tag" = {
       wantedBy = [ "timers.target" ];
       timerConfig = {
         OnCalendar = "hourly";
         OnBootSec = "5m";
-        Unit = "qbit-tag.service";
+        Unit = "qbtools-tag.service";
       };
     };
 
-    systemd.services."qbit-tag" =
+    systemd.services."qbtools-tag" =
 
       {
         script = ''
@@ -44,7 +44,71 @@ with lib;
         path = [ pkgs.podman ];
       };
 
+    systemd.timers."qbtools-prune-orphaned" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "daily";
+        OnBootSec = "5m";
+        Unit = "qbtools-prune-orphaned.service";
+      };
+    };
 
+    systemd.services."qbtools-prune-orphaned" =
+
+      {
+        script = ''
+          ${pkgs.podman}/bin/podman run --rm \
+          -v ${config.sops.secrets."services/qbittorrent/config.yaml".path}:/config/config.yaml \
+          ${image} \
+          prune  \
+          --exclude-category  \
+          manual \
+          --exclude-category \
+          uploads \
+          --include-tag \
+          unregistered \
+          --server https://qbittorrent.trux.dev  \
+          --port 443  \
+          --config /config/config.yaml
+        '';
+        path = [ pkgs.podman ];
+      };
+
+    systemd.timers."qbtools-prune-expired" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "daily";
+        OnBootSec = "5m";
+        Unit = "qbtools-prune-expired.service";
+      };
+    };
+
+    systemd.services."qbtools-prune-expired" =
+
+      {
+        script = ''
+          ${pkgs.podman}/bin/podman run --rm \
+          -v ${config.sops.secrets."services/qbittorrent/config.yaml".path}:/config/config.yaml \
+          ${image} \
+          prune  \
+          --exclude-category  \
+          manual \
+          --exclude-category \
+          uploads \
+          --include-tag \
+          expired \
+          --exclude-tag \
+          activity:24h \
+          --exclude-tag \
+          permaseed \
+          --exclude-tag \
+          site:myanonamouse \
+          --server https://qbittorrent.trux.dev  \
+          --port 443  \
+          --config /config/config.yaml
+        '';
+        path = [ pkgs.podman ];
+      };
 
   };
 }
