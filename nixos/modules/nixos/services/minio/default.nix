dfe0b62375c666eae4f29f124089df28cea89555
Truxnell <9149206+truxnell@users.noreply.github.com>
2024-05-14 14:26:34 +1000

    feat: minio

---
+++ nixos/modules/nixos/services/minio/default.nix
@@ -0,0 +1,173 @@
+{ lib
+, config
+, pkgs
+, ...
+}:
+with lib;
+let
+  cfg = config.mySystem.${category}.${app};
+  app = "minio";
+  category = "services";
+  description = "s3 compatiable storage";
+  # image = "";
+  user = "kah"; #string
+  group = "kah"; #string
+  port = 9001; #int
+  s3port = 9000; #int
+  appFolder = "/var/lib/${app}";
+  # persistentFolder = "${config.mySystem.persistentFolder}/var/lib/${appFolder}";
+  host = "${app}" + (if cfg.dev then "-dev" else "");
+  url = "${host}.${config.networking.domain}";
+in
+{
+  options.mySystem.${category}.${app} =
+    {
+      enable = mkEnableOption "${app}";
+      addToHomepage = mkEnableOption "Add ${app} to homepage" // { default = true; };
+      monitor = mkOption
+        {
+          type = lib.types.bool;
+          description = "Enable gatus monitoring";
+          default = true;
+        };
+      prometheus = mkOption
+        {
+          type = lib.types.bool;
+          description = "Enable prometheus scraping";
+          default = true;
+        };
+      addToDNS = mkOption
+        {
+          type = lib.types.bool;
+          description = "Add to DNS list";
+          default = true;
+        };
+      dev = mkOption
+        {
+          type = lib.types.bool;
+          description = "Development instance";
+          default = false;
+        };
+      backup = mkOption
+        {
+          type = lib.types.bool;
+          description = "Enable backups";
+          default = true;
+        };
+
+
+
+    };
+
+  config = mkIf cfg.enable {
+
+    ## Secrets
+    sops.secrets."${category}/${app}/env" = {
+      sopsFile = ./secrets.sops.yaml;
+      owner = user;
+      group = group;
+      restartUnits = [ "${app}.service" ];
+    };
+
+    users.users.truxnell.extraGroups = [ group ];
+
+
+    # Folder perms - only for containers
+    # systemd.tmpfiles.rules = [
+    # "d ${appFolder}/ 0750 ${user} ${group} -"
+    # ];
+
+    environment.persistence."${config.mySystem.system.impermanence.persistPath}" = lib.mkIf config.mySystem.system.impermanence.enable {
+      directories = [{ directory = appFolder; inherit user; inherit group; mode = "750"; }];
+    };
+
+
+    ## service
+
+
+    services.minio = {
+      enable = true;
+      listenAddress = "0.0.0.0:${builtins.toString s3port}";
+      consoleAddress = "0.0.0.0:${builtins.toString port}";
+      region = "us-east-1";
+      rootCredentialsFile = "${config.sops.secrets."${category}/${app}/env".path}";
+      dataDir = [ "${config.mySystem.nasFolder}/minio" ];
+      configDir = "/var/lib/${app}";
+    };
+
+    systemd.services.minio = {
+      environment = {
+        MINIO_SERVER_URL = "https://s3.trux.dev";
+        MINIO_BROWSER_REDIRECT_URL = "https://minio.trux.dev";
+      };
+    };
+
+
+    # homepage integration
+    mySystem.services.homepage.infrastructure = mkIf cfg.addToHomepage [
+      {
+        ${app} = {
+          icon = "${app}.svg";
+          href = "https://${url}";
+          inherit description;
+        };
+      }
+    ];
+
+    ### gatus integration
+    mySystem.services.gatus.monitors = mkIf cfg.monitor [
+      {
+        name = app;
+        group = "${category}";
+        url = "https://${url}";
+        interval = "1m";
+        conditions = [ "[CONNECTED] == true" "[STATUS] == 200" "[RESPONSE_TIME] < 50" ];
+      }
+    ];
+
+    ### Ingress
+    services.nginx.virtualHosts.${url} = {
+      forceSSL = true;
+      useACMEHost = config.networking.domain;
+      locations."^~ /" = {
+        proxyPass = "http://127.0.0.1:${builtins.toString port}";
+      };
+    };
+    services.nginx.virtualHosts."s3.${config.networking.domain}" = {
+      forceSSL = true;
+      useACMEHost = config.networking.domain;
+      locations."^~ /" = {
+        proxyPass = "http://127.0.0.1:${builtins.toString s3port}";
+      };
+    };
+
+
+    ### firewall config
+
+    networking.firewall.interfaces.wg0.allowedTCPPorts = [
+      9000
+      9001
+    ];
+
+    ### backups
+    warnings = [
+      (mkIf (!cfg.backup && config.mySystem.purpose != "Development")
+        "WARNING: Backups for ${app} are disabled!")
+    ];
+
+    services.restic.backups = mkIf cfg.backup (config.lib.mySystem.mkRestic
+      {
+        inherit app user;
+        paths = [ appFolder ];
+        inherit appFolder;
+      });
+
+
+    # services.postgresqlBackup = {
+    #   databases = [ app ];
+    # };
+
+
+
+  };
+}
