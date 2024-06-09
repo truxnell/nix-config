9e9472bd489c55d5cd8174a38e19cf1550871abd
Truxnell <9149206+truxnell@users.noreply.github.com>
2024-06-09 20:29:01 +1000

    fix: bump n stuff

--- nixos/modules/nixos/containers/immich/default.nix
+++ nixos/modules/nixos/containers/immich/default.nix
@@ -12,14 +12,15 @@ let
   # image = "";
   user = "kah"; #string
   group = "kah"; #string
-  port = 8080; #int
+  port = 3001; #int
   appFolder = "/var/lib/${app}";
   # persistentFolder = "${config.mySystem.persistentFolder}/var/lib/${appFolder}";
   host = "${app}" + (if cfg.dev then "-dev" else "");
   url = "${host}.${config.networking.domain}";
   environment = {
-    DB_DATA_LOCATION = "/run/postgresql";
+    DB_HOSTNAME = "immich-postgres";
     REDIS_HOSTNAME = "immich-redis";
+    IMMICH_METRICS = "true";
   };
 in
 {
@@ -99,6 +100,9 @@ in
     # Folder perms - only for containers
     systemd.tmpfiles.rules = [
       "d ${appFolder}/ 0750 ${user} ${group} -"
+      "d ${appFolder}/machine-learning 0750 ${user} ${group} -"
+      "d ${appFolder}/postgres 0750 ${user} ${group} -"
+
     ];
 
     environment.persistence."${config.mySystem.system.impermanence.persistPath}" = lib.mkIf config.mySystem.system.impermanence.enable {
@@ -107,29 +111,44 @@ in
 
     virtualisation.oci-containers.containers =
       {
-        # immich-server = {
-        #   image = "ghcr.io/immich-app/immich-server:v1.105.1";
-        #   cmd = [ "start.sh" "immich" ];
-        #   inherit environment;
-        #   volumes = [
-        #     "/run/redis-immich:/run/redis-immich"
-        #     "/etc/localtime:/etc/localtime:ro"
-        #     "${config.mySystem.nasFolder}/photos/upload:/usr/src/app/upload"
-        #   ];
-        #   dependsOn = [ "podman-immich-redis.service" "podman-immich-postgres.service" ];
-        # };
+        immich-server = {
+          image = "ghcr.io/immich-app/immich-server:v1.105.1";
+          cmd = [ "start.sh" "immich" ];
+          environmentFiles = [ config.sops.secrets."${category}/${app}/env".path ];
+          inherit environment;
+          volumes = [
+            "/etc/localtime:/etc/localtime:ro"
+            "${config.mySystem.nasFolder}/photos/upload:/usr/src/app/upload"
+          ];
+          dependsOn = [ "immich-redis" "immich-postgres" ];
+          ports = [ "${builtins.toString port}:${builtins.toString port}" ];
+          extraOptions = [
+            # Force DNS resolution to only be the podman dnsname name server; by default podman provides a resolv.conf
+            # that includes both this server and the upstream system server, causing resolutions of other pod names
+            # to be inconsistent.
+            "--dns=10.88.0.1"
+          ];
+        };
 
-        # immich-micoservices = {
-        #   image = "ghcr.io/immich-app/immich-server:v1.105.1";
-        #   cmd = [ "start.sh" "microservices" ];
-        #   inherit environment;
-        #   volumes = [
-        #     "/run/redis-immich:/run/redis-immich"
-        #     "/etc/localtime:/etc/localtime:ro"
-        #     "${config.mySystem.nasFolder}/photos/upload:/usr/src/app/upload"
-        #   ];
-        #   dependsOn = [ "podman-immich-redis.service" "podman-immich-postgres.service" ];
-        # };
+        immich-micoservices = {
+          image = "ghcr.io/immich-app/immich-server:v1.105.1";
+          cmd = [ "start.sh" "microservices" ];
+          environmentFiles = [ config.sops.secrets."${category}/${app}/env".path ];
+          inherit environment;
+          volumes = [
+            "/etc/localtime:/etc/localtime:ro"
+            "${config.mySystem.nasFolder}/photos/upload:/usr/src/app/upload"
+          ];
+          dependsOn = [ "immich-redis" "immich-postgres" ];
+          extraOptions = [
+            # Force DNS resolution to only be the podman dnsname name server; by default podman provides a resolv.conf
+            # that includes both this server and the upstream system server, causing resolutions of other pod names
+            # to be inconsistent.
+            "--dns=10.88.0.1"
+            "--device=/dev/dri:/dev/dri"
+          ];
+
+        };
 
 
         immich-machine-learning = {
@@ -140,6 +159,13 @@ in
             "${config.mySystem.nasFolder}/photos/upload:/usr/src/app/upload"
             "/var/lib/immich/machine-learning:/cache"
           ];
+          extraOptions = [
+            # Force DNS resolution to only be the podman dnsname name server; by default podman provides a resolv.conf
+            # that includes both this server and the upstream system server, causing resolutions of other pod names
+            # to be inconsistent.
+            "--dns=10.88.0.1"
+          ];
+
         };
 
         immich-redis = {
@@ -206,7 +232,6 @@ in
       useACMEHost = config.networking.domain;
       locations."^~ /" = {
         proxyPass = "http://127.0.0.1:${builtins.toString port}";
-        extraConfig = "resolver 10.88.0.1;";
       };
     };
 
@@ -217,23 +242,40 @@ in
     #   allowedUDPPorts = [ port ];
     # };
 
+    services.vmagent = {
+      prometheusConfig = {
+        scrape_configs = [
+          {
+            job_name = "immich";
+            # scrape_timeout = "40s";
+            static_configs = [
+              {
+                targets = [ "http://127.0.0.1:${builtins.toString port}" ];
+              }
+            ];
+          }
+        ];
+      };
+    };
+
+
     ### backups
     warnings = [
       (mkIf (!cfg.backup && config.mySystem.purpose != "Development")
         "WARNING: Backups for ${app} are disabled!")
     ];
 
-    services.restic.backups = mkIf cfg.backup (config.lib.mySystem.mkRestic
-      {
-        inherit app user;
-        paths = [ appFolder ];
-        inherit appFolder;
-      });
+    # services.restic.backups = mkIf cfg.backup (config.lib.mySystem.mkRestic
+    #   {
+    #     inherit app user;
+    #     paths = [ appFolder ];
+    #     inherit appFolder;
+    #   });
 
 
-    services.postgresqlBackup = {
-      databases = [ app ];
-    };
+    # services.postgresqlBackup = {
+    #   databases = [ app ];
+    # };
 
 
 
