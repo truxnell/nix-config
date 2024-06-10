97711183dd2986b0010ab6d70f529b7cad5ff645
Truxnell <9149206+truxnell@users.noreply.github.com>
2024-06-10 17:04:07 +1000

    fix: hostid

--- nixos/hosts/daedalus/default.nix
+++ nixos/hosts/daedalus/default.nix
@@ -63,7 +63,7 @@
     };
 
     networking.hostName = "daedalus"; # Define your hostname.
-    networking.hostId = "ed3980cb"; # for zfs, helps stop importing to wrong machine
+    networking.hostId = "485cafad"; # for zfs, helps stop importing to wrong machine
     networking.useDHCP = lib.mkDefault true;
 
     fileSystems."/" =
