37577fd62ed9ebca6a0c224f7d35a6d9d031ad36
truxnell <19149206+truxnell@users.noreply.github.com>
2024-09-03 22:19:32 +1000

    feat: add zfs nfs

--- nixos/hosts/daedalus/storage.nix
+++ nixos/hosts/daedalus/storage.nix
@@ -185,6 +185,7 @@ in
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
     /tank 10.8.10.1/24(no_subtree_check,all_squash,anonuid=568,anongid=568,rw,fsid=0) 10.8.20.1/24(no_subtree_check,all_squash,anonuid=568,anongid=568,rw,fsid=0)
+    /zfs 10.8.10.1/24(no_subtree_check,all_squash,anonuid=568,anongid=568,rw,fsid=0) 10.8.20.1/24(no_subtree_check,all_squash,anonuid=568,anongid=568,rw,fsid=0)
   '';
   # disable v2/v3 nfs to force v4
   services.nfs.settings.nfsd = {
