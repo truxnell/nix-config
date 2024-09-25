38c83dd335170a9486c2e0cec685365145afb5c0
Truxnell <9149206+truxnell@users.noreply.github.com>
2024-09-25 21:52:04 +1000

    temp

--- nixos/home/modules/programs/thunderbird.nix
+++ nixos/home/modules/programs/thunderbird.nix
@@ -15,20 +15,20 @@ in
       enable = mkEnableOption "${app}";
     };
 
-  config.programs.${app} = mkIf cfg.enable {
+  # config.programs.${app} = mkIf cfg.enable {
 
-    enable = true;
-    package = pkgs.unstable.thunderbird;
-    profiles = {
-      "main" = {
-        isDefault = true;
-        settings = {
-          "calendar.alarms.showmissed" = false;
-          "calendar.alarms.playsound" = false;
-          "calendar.alarms.show" = false;
-        };
-      };
-    };
-  };
+  #   enable = true;
+  #   package = pkgs.unstable.thunderbird;
+  #   profiles = {
+  #     "main" = {
+  #       isDefault = true;
+  #       settings = {
+  #         "calendar.alarms.showmissed" = false;
+  #         "calendar.alarms.playsound" = false;
+  #         "calendar.alarms.show" = false;
+  #       };
+  #     };
+  #   };
+  # };
 
 }
