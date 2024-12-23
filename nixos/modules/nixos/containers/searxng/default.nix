04b0021bc41aefd798de13bd2c4ca03f86135dbe
truxnell <19149206+truxnell@users.noreply.github.com>
2024-12-23 16:47:17 +1100

    feat: update searx and add json for llm

--- nixos/modules/nixos/containers/searxng/default.nix
+++ nixos/modules/nixos/containers/searxng/default.nix
@@ -9,12 +9,10 @@ let
   image = "docker.io/searxng/searxng:2023.11.1-b5a8ddfec";
   user = "977"; #string
   group = "977"; #string
-  port = 8080; #int
+  port = 8084; #int
   cfg = config.mySystem.services.${app};
   appFolder = "/var/lib/${app}";
   # persistentFolder = "${config.mySystem.persistentFolder}/var/lib/${appFolder}";
-  configNix = { use_default_settings = { engines = { keep_only = [ "arch linux wiki" "google" "google images" "google news" "google videos" "google scholar" "google play apps" "duckduckgo" "brave" "startpage" "gitlab" "github" "codeberg" "sourcehut" "bitbucket" "apple app store" "wikipedia" "currency" "docker hub" "ddg definitions" "duckduckgo images" "bandcamp" "deviantart" "tineye" "apple maps" "fdroid" "flickr" "free software directory" "z-library" "lobste.rs" "azlyrics" "openstreetmap" "npm" "pypi" "lib.rs" "nyaa" "reddit" "sepiasearch" "soundcloud" "stackoverflow" "askubuntu" "superuser" "searchcode code" "unsplash" "youtube" "wolframalpha" "mojeek" ]; }; }; engines = [{ name = "brave"; disabled = false; } { name = "startpage"; disabled = false; } { name = "apple app store"; disabled = false; } { name = "ddg definitions"; disabled = false; } { name = "tineye"; disabled = false; } { name = "apple maps"; disabled = false; } { name = "duckduckgo images"; disabled = false; } { name = "fdroid"; disabled = false; } { name = "free software directory"; disabled = false; } { name = "bitbucket"; disabled = false; } { name = "gitlab"; disabled = false; } { name = "codeberg"; disabled = false; } { name = "google play apps"; disabled = false; } { name = "lobste.rs"; disabled = false; } { name = "azlyrics"; disabled = false; } { name = "npm"; disabled = false; } { name = "nyaa"; disabled = false; categories = "videos"; } { name = "searchcode code"; disabled = false; } { name = "mojeek"; disabled = false; } { name = "lib.rs"; disabled = false; } { name = "sourcehut"; disabled = false; }]; general = { instance_name = "NatFlix Search"; enable_metrics = false; }; brand = { new_issue_url = ""; docs_url = ""; public_instances = ""; wiki_url = ""; issue_url = ""; }; search = { safe_search = 0; autocomplete = "duckduckgo"; autocomplete_min = 2; default_lang = "en"; max_page = 0; }; server = { base_url = "https://searxng.\${EXTERNAL_DOMAIN}/"; image_proxy = true; http_protocol_version = "1.1"; method = "GET"; }; ui = { static_use_hash = true; infinite_scroll = true; default_theme = "simple"; theme_args = { simple_style = "dark"; }; }; enabled_plugins = [ "Hash plugin" "Search on category select" "Self Information" "Tracker URL remover" "Open Access DOI rewrite" "Vim-like hotkeys" ]; };
-  configFile = builtins.toFile "config.yaml" (builtins.toJSON configNix);
 in
 {
   options.mySystem.services.${app} =
@@ -25,38 +23,215 @@ in
 
   config = mkIf cfg.enable {
 
-    virtualisation.oci-containers.containers.${app} = {
-      image = "${image}";
-      user = "568:568";
-      volumes = [
-        "${configFile}:/config/config.yaml:ro"
-        "/etc/localtime:/etc/localtime:ro"
-      ];
-      environment = {
-        TZ = "${config.time.timeZone}";
-        SEARXNG_BASE_URL = "https://searxng.${config.mySystem.domain}/";
-        SEARXNG_URL = "https://searxng.${config.mySystem.domain}";
-      };
-      extraOptions = [
-        "--read-only"
-        "--tmpfs=/etc/searxng/"
-        "--cap-add=CHOWN"
-        "--cap-add=SETGID"
-        "--cap-add=SETUID"
-        "--cap-add=DAC_OVERRIDE"
-      ];
+  services.searx = {
+    enable = true;
+    # environmentFile = config.sops.secrets.searxng_env_file.path;
+    redisCreateLocally = true;
 
+    # Rate limiting
+    # limiterSettings = {
+    #   real_ip = {
+    #     x_for = 1;
+    #     ipv4_prefix = 32;
+    #     ipv6_prefix = 56;
+    #   };
+    #
+    #   botdetection = {
+    #     ip_limit = {
+    #       filter_link_local = true;
+    #       link_token = true;
+    #     };
+    #   };
+    # };
+
+
+    # Searx configuration
+    settings = {
+      general = {
+        contact_url = false;
+        debug = false;
+        donation_url = false;
+        enable_metrics = true;
+        instance_name = "Natflix search";
+        privacypolicy_url = false;
+      };
+
+      ui = {
+        center_alignment = true;
+        default_locale = "en";
+        default_theme = "simple";
+        hotkeys = "vim";
+        infinite_scroll = false;
+        query_in_title = true;
+        search_on_category_select = true;
+        static_use_hash = true;
+        theme_args.simple_style = "dark";
+      };
+
+      # Search engine settings
+      search = {
+        autocomplete = "startpage";
+        autocomplete_min = 2;
+        ban_time_on_fail = 5;
+        default_lang = "en";
+        formats = [
+          "html"
+          "json"
+        ];
+        max_ban_time_on_fail = 120;
+        safe_search = 0;
+      };
+
+      # Server configuration
+      server = {
+        base_url = "https://searxng.trux.dev";
+        bind_address = "127.0.0.1";
+        image_proxy = true;
+        # limiter = true;
+        limiter = false;
+        method = "GET";
+        public_instance = false;
+        port = 8084;
+        secret_key = "loljknahplz";
+      };
+
+      # Search engines
+      engines = lib.mapAttrsToList (name: value: {inherit name;} // value) {
+        "1x".disabled = true;
+        "artic".disabled = true;
+        "bing images".disabled = false;
+        "bing videos".disabled = false;
+        "bing".disabled = false;
+        # "brave".disabled = false;
+        # "brave.images".disabled = true;
+        # "brave.news".disabled = true;
+        # "brave.videos".disabled = true;
+
+        "crowdview" = {
+          disabled = false;
+          weight = 0.5;
+        };
+
+        "curlie".disabled = true;
+        "currency".disabled = true;
+        "dailymotion".disabled = true;
+
+        "ddg definitions" = {
+          disabled = false;
+          weight = 2;
+        };
+
+        "deviantart".disabled = false;
+        "dictzone".disabled = true;
+        "duckduckgo images".disabled = true;
+        "duckduckgo videos".disabled = true;
+        "duckduckgo".disabled = true;
+        "flickr".disabled = true;
+        "google images".disabled = false;
+        "google news".disabled = true;
+        "google play movies".disabled = true;
+        "google videos".disabled = false;
+        "imgur".disabled = false;
+        "invidious".disabled = true;
+        "library of congress".disabled = false;
+        "lingva".disabled = true;
+
+        "material icons" = {
+          disabled = true;
+          weight = 0.2;
+        };
+
+        "mojeek".disabled = true;
+        "mwmbl" = {
+          disabled = false;
+          weight = 0.4;
+        };
+
+        "odysee".disabled = true;
+        "openverse".disabled = false;
+        "peertube".disabled = false;
+        "pinterest".disabled = true;
+        "piped".disabled = true;
+        "qwant images".disabled = true;
+        "qwant videos".disabled = false;
+        "qwant".disabled = true;
+        "rumble".disabled = false;
+        "sepiasearch".disabled = false;
+        "svgrepo".disabled = false;
+        "unsplash".disabled = false;
+        "vimeo".disabled = true;
+
+        "wallhaven" = {
+          disabled = false;
+          api_key = "@WALLHAVEN_API_KEY@";
+          safesearch_map = "0";
+        };
+
+        "wikibooks".disabled = false;
+        "wikicommons.images".disabled = false;
+        "wikidata".disabled = false;
+        "wikiquote".disabled = true;
+        "wikisource".disabled = true;
+
+        "wikispecies" = {
+          disabled = false;
+          weight = 0.5;
+        };
+
+        "wikiversity" = {
+          disabled = false;
+          weight = 0.5;
+        };
+
+        "wikivoyage" = {
+          disabled = false;
+          weight = 0.5;
+        };
+
+        "yacy images".disabled = true;
+        "youtube".disabled = false;
+      };
+
+      # Outgoing requests
+      outgoing = {
+        request_timeout = 5.0;
+        max_request_timeout = 15.0;
+        pool_connections = 100;
+        pool_maxsize = 15;
+        enable_http2 = true;
+      };
+
+      # Enabled plugins
+      enabled_plugins = [
+        "Basic Calculator"
+        "Hash plugin"
+        "Tor check plugin"
+        "Open Access DOI rewrite"
+        "Hostnames plugin"
+        "Unit converter plugin"
+        "Tracker URL remover"
+      ];
     };
+  };
+    
 
     services.nginx.virtualHosts."${app}.${config.networking.domain}" = {
       useACMEHost = config.networking.domain;
       forceSSL = true;
-      locations."^~ /" = {
-        proxyPass = "http://${app}:${builtins.toString port}";
-        extraConfig = "resolver 10.88.0.1;";
+      locations = {
+        "/" = {
+          proxyPass = "http://127.0.0.1:8084";
+        };
 
+        "/api/" = {
+          proxyPass = "http://127.0.0.1:8084";
+          extraConfig = ''
+            rewrite /api/ /search?format=json break;
+          '';
+        };
       };
     };
+    
 
     mySystem.services.homepage.home = mkIf cfg.addToHomepage [
       {
