484f4e22636c8f97f270a8021b375f634df5d729
truxnell <19149206+truxnell@users.noreply.github.com>
2026-02-01 18:13:29 +1100

    fix zfs?

--- nixos/hosts/shodan/default.nix
+++ nixos/hosts/shodan/default.nix
@@ -150,26 +150,26 @@
   fileSystems."/" = {
     device = "rpool/local/root";
     fsType = "zfs";
-    options = [ "zfsutil" ]
+    options = [ "zfsutil" ];
   };
 
   fileSystems."/nix" = {
     device = "rpool/local/nix";
     fsType = "zfs";
-    options = [ "zfsutil" ]
+    options = [ "zfsutil" ];
   };
 
   fileSystems."/persist" = {
     device = "rpool/safe/persist";
     fsType = "zfs";
-    options = [ "zfsutil" ]
+    options = [ "zfsutil" ];
     neededForBoot = true; # for impermanence
   };
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/76FA-78DF";
     fsType = "vfat";
-    options = [ "zfsutil" ]
+    options = [ "zfsutil" ];
     options = [
       "fmask=0022"
       "dmask=0022"
