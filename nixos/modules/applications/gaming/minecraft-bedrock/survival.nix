891dac992066b1c2017cd2a12fbb6b56727b228f
truxnell <19149206+truxnell@users.noreply.github.com>
2026-02-25 18:46:47 +1100

    feat: cheatz lol

--- nixos/modules/applications/gaming/minecraft-bedrock/survival.nix
+++ nixos/modules/applications/gaming/minecraft-bedrock/survival.nix
@@ -105,7 +105,7 @@ in
         LEVEL_NAME = instance;
         TEXTUREPACK_REQUIRED = "false";
         WHITE_LIST = "false";
-        ALLOW_CHEATS = "false";
+        ALLOW_CHEATS = "true";
         SERVER_PORT="${builtins.toString port}";
       };
       # environmentFiles = [ config.sops.secrets."services/${app}/env".path ];
